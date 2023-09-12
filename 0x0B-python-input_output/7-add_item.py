b9368c202f32ccc324ca4ec19aa08f7f27ae276d
Peter <peterkasinamunda@gmail.com>
2023-09-12 09:59:28 +0000

    using input_output ops

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""Module 9"""
+"""loads adds saves json to file"""
+import json
 import sys
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-
 try:
-    lista = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    lista = []
-for i in range(1, len(sys.argv)):
-    lista.append(sys.argv[i])
-save_to_json_file(lista, "add_item.json")
+    jsonList = load_from_json_file("add_item.json")
+except:
+    jsonList = []
+
+for i in sys.argv[1:]:
+    jsonList.append(i)
+save_to_json_file(jsonList, "add_item.json")
