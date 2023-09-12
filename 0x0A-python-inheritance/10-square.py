c47c7ff1a952930389c346a3171512630cf92ddf
Peter <peterkasinamunda@gmail.com>
2023-09-12 08:55:59 +0000

    fixing

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,55 +1,19 @@
 #!/usr/bin/python3
-""" Module 11-base_geometry"""
-
-
-class BaseGeometry:
-    """Class BaseGeometry"""
-    def area(self):
-        """Area method"""
-        raise Exception('area() is not implemented')
-
-    def integer_validator(self, name, value):
-        """integer_validator function"""
-        if type(value) != int:
-            raise TypeError(name + ' must be an integer')
-        if value <= 0:
-            raise ValueError(name + ' must be greater than 0')
-
-
-class Rectangle(BaseGeometry):
-    """Class BaseGeometry"""
-
-    def __init__(self, width, height):
-        """Init"""
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """Find the Area"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """Overcharge __str__"""
-        string = "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
-        return string
+# 10-square.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Defines a Rectangle subclass Square."""
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Class Square"""
+    """Represent a square."""
 
     def __init__(self, size):
-        """Init Square"""
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
         self.integer_validator("size", size)
-        self.__size = size
         super().__init__(size, size)
-
-    def area(self):
-        """Find the area"""
-        return self.__size ** 2
-
-    def __str__(self):
-        """Overcharge __str__"""
-        string = "[Square] " + str(self.__size) + "/" + str(self.__size)
-        return string
+        self.__size = size
