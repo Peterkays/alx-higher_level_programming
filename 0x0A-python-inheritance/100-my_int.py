4d092e01809339bc4bd3b4b3b148c67c1f8ae674
Peter <peterkasinamunda@gmail.com>
2023-09-12 06:24:20 +0000

    fixing: my_int

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
+"""Module 100-my_int"""
+
+
 class MyInt(int):
-    def __eq__(self, val2):
-        return self - val2 is not 0
+    """Class MyInt"""
 
-    def __ne__(self, val2):
-        return self - val2 is 0
+    def __init__(self, value):
+        """Init method"""
+        self.value = value
 
+    def __eq__(self, other):
+        """Rebel equal"""
+        if isinstance(other, MyInt):
+            return self.value == other.value
+        return False
+
+    def __ne__(self, other):
+        """Rebel not equal"""
+        return not self.__eq__(other)
